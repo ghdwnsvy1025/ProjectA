9a1c892476174f381e6c68ec101ef7f2403108de
DESKTOP-QGM8IET\HongJunpyo <xhql1034@naver.com>
2024-11-22 12:40:50 +0900

    git test

--- Source/ProjectA/Item/PAItem.cpp
+++ Source/ProjectA/Item/PAItem.cpp
@@ -3,9 +3,11 @@
 
 #include "Item/PAItem.h"
 
+#include "ProjectA.h"
+
 APAItem::APAItem()
 {
-
+	PA_LOG(LogTest, Log, TEXT("Text"));
 }
 
 UAbilitySystemComponent* APAItem::GetAbilitySystemComponent() const
